5d8f555292b98794443ebf4ae08a39bf498b0208
cuentasena <josedavidlemusrestrepo5@gmail.com>
2026-05-29 10:36:07 -0500

    feat: creacion product list , su scss , html y ts

---
+++ src/app/modules/products/components/product-list/product-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+  productos: Product[] = [];
+  terminoBusqueda: string = '';
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.actualizarLista();
+  }
+
+  actualizarLista(): void {
+    this.productos = this.productService.buscarProductos(this.terminoBusqueda);
+  }
+
+  buscar(): void {
+    this.actualizarLista();
+  }
+
+  limpiarBusqueda(): void {
+    this.terminoBusqueda = '';
+    this.actualizarLista();
+  }
+}
